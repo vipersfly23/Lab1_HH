a5890e4860979847c7b6e05ce73879932d17b200
Hunter Her <c16hunter.her@usafa.edu>
2014-01-22 20:24:29 -0700

    Adding vhdl

---
+++ Lab1_HH_Module.vhd
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------------------
+-- Company: USAF Academy	
+-- Engineer: C3C Her, Hunter B
+-- 
+-- Create Date:    14:13:27 01/21/2014 
+-- Design Name: 	Binary 2's complement
+-- Module Name:    Lab1_HH_Module - Behavioral 
+-- Project Name:	2's Complement Converter 
+-- Target Devices: Nexys2
+-- Tool versions: 
+-- Description: The objective of this is to perform the 2's complement of the number
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Lab1_HH_Module is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           C : in  STD_LOGIC;
+           D : out  STD_LOGIC;
+           E : out  STD_LOGIC;
+           F : out  STD_LOGIC);
+end Lab1_HH_Module;
+
+architecture Behavioral of Lab1_HH_Module is
+
+begin
+
+D <= (not A and not B and C) or (not A and B) or (A and not B and not C);
+E<= (not B and C )or (B and not C);
+F<= C;
+
+
+end Behavioral;
+
---
+++ Lab1_HH_TestBench.vhd
@@ -0,0 +1,146 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   14:20:56 01/21/2014
+-- Design Name:   
+-- Module Name:   C:/Users/C16Hunter.Her/Desktop/Academic_Spring_2014/ECE_281/Lab1/Lab1HH/Lab1_HH_TestBench.vhd
+-- Project Name:  Lab1HH
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Lab1_HH_Module
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Lab1_HH_TestBench IS
+END Lab1_HH_TestBench;
+ 
+ARCHITECTURE behavior OF Lab1_HH_TestBench IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Lab1_HH_Module
+    PORT(
+         A : IN  std_logic;
+         B : IN  std_logic;
+         C : IN  std_logic;
+         D : OUT  std_logic;
+         E : OUT  std_logic;
+         F : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic := '0';
+   signal B : std_logic := '0';
+   signal C : std_logic := '0';
+
+ 	--Outputs
+   signal D : std_logic;
+   signal E : std_logic;
+   signal F : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+  
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Lab1_HH_Module PORT MAP (
+          A => A,
+          B => B,
+          C => C,
+          D => D,
+          E => E,
+          F => F
+        );
+
+   
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+
+     
+
+      -- insert stimulus here 
+		   -- First line of truth table
+		A <= '0';
+		B <= '0';
+		C <= '0';
+		wait for 100 ns;	
+		
+		-- Second line of truth table
+		A <= '0';
+		B <= '0';
+		C <= '1';
+		wait for 100 ns;	
+		
+		-- Complete_ the rest of the truth table below:
+		
+			-- ThirdSecond line of truth table
+		A <= '0';
+		B <= '1';
+		C <= '0';
+		wait for 100 ns;	
+		
+			-- Fourth line of truth table
+		A <= '0';
+		B <= '1';
+		C <= '1';
+		wait for 100 ns;	
+		
+			-- Fifth line of truth table
+		A <= '1';
+		B <= '0';
+		C <= '0';
+		wait for 100 ns;	
+		
+			-- Sixth line of truth table
+		A <= '1';
+		B <= '0';
+		C <= '1';
+		wait for 100 ns;	
+		
+			-- Seventh line of truth table
+		A <= '1';
+		B <= '1';
+		C <= '0';
+		wait for 100 ns;	
+		
+			-- Eight line of truth table
+		A <= '1';
+		B <= '1';
+		C <= '1';
+		wait for 100 ns;	
+
+      wait;
+   end process;
+
+END;
